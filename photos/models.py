72f1d2e959515db653033f67c3183b0ff4274649
Jerry-Wemisiko <jerry.wemisiko@student.moringaschool.com>
2021-09-05 01:04:50 +0300

    Image model test

--- photos/models.py
+++ photos/models.py
@@ -19,6 +19,7 @@ class Image(models.Model):
     description = models.CharField(max_length=100)
     location = models.ForeignKey(Location,on_delete=models.CASCADE)
     category = models.ManyToManyField(Category)
+    image = models.ImageField()
 
     def save_image(self):
         self.save()
--- photos/tests.py
+++ photos/tests.py
@@ -2,9 +2,18 @@ from django.test import TestCase
 from .models import Image,Location,Category
 
 # Create your tests here.
-class LocationTest(TestCase):
+
+class ImageTest(TestCase):
+   
     def setUp(self):
-        self.nairobi = Location(location = 'Nairobi')
+        self.funny = Image(name ='Jerry', description = 'party mode',location = 'New York',category = 'Fun')
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.nairobi,Location))
+        self.assertTrue(isinstance(self.funny,Image))
+
+    def test_save_method(self):
+        self.funny.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0)
+
+    
