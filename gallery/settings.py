9fcf4feb5011fbd1dcfe775731d8329cacb5a95b
Jerry-Wemisiko <jerry.wemisiko@student.moringaschool.com>
2021-09-05 00:33:38 +0300

    create new models

--- gallery/settings.py
+++ gallery/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'photos'
+    'photos',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gallery',
+        'USER':'moringa',
+        'PASSWORD': 'skyles'
+
     }
 }
 
@@ -106,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    location = models.CharField(max_length=30)
+
+
+class Category(models.Model):
+    category = models.CharField(max_length=30)
+
+class Image(models.Model):
+    name = models.CharField(max_length=15)
+    description = models.CharField(max_length=100)
+    
