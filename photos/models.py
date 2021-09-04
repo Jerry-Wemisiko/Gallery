58c4d0c5c0a1773b82e8b7595174b5102e2f7c37
Jerry-Wemisiko <jerry.wemisiko@student.moringaschool.com>
2021-09-05 00:42:31 +0300

    Update save method on Image

--- photos/models.py
+++ photos/models.py
@@ -4,13 +4,25 @@ from django.db import models
 class Location(models.Model):
     location = models.CharField(max_length=30)
 
+    def __str__(self) -> str:
+        return self.location
+
 
 class Category(models.Model):
     category = models.CharField(max_length=30)
+
+    def __str__(self) -> str:
+        return self.category
 
 class Image(models.Model):
     name = models.CharField(max_length=15)
     description = models.CharField(max_length=100)
     location = models.ForeignKey(Location,on_delete=models.CASCADE)
     category = models.ManyToManyField(Category)
+
+    def save_image(self):
+        self.save()
+
+    
+
     
