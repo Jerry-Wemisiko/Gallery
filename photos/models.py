b0fef3d4a5d183f2cfcd5f027089013c372c92f2
Jerry-Wemisiko <jerry.wemisiko@student.moringaschool.com>
2021-09-09 18:24:21 +0300

    fixed migrations

--- photos/models.py
+++ photos/models.py
@@ -54,7 +54,7 @@ class Image(models.Model):
     name = models.CharField(max_length=15)
     description = models.CharField(max_length=100)
     location = models.ForeignKey(Location,on_delete=models.CASCADE)
-    category = models.ForeignKey(Category,on_delete=models.CASCADE ,blank= True)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE ,blank=True)
     image = CloudinaryField('image' ,default= "Pic")
 
 
