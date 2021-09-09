f04f552ce5335b09f805d42ff4b8dda59ae9287d
Jerry-Wemisiko <jerry.wemisiko@student.moringaschool.com>
2021-09-09 12:52:46 +0300

    fixed heroku error

--- gallery/settings.py
+++ gallery/settings.py
@@ -42,15 +42,16 @@ DEBUG = environ.get('DEBUG')
 # Application definition
 
 INSTALLED_APPS = [
+    'photos',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'photos',
-    'bootstrap4',
     'cloudinary',
+    'bootstrap4',
+    
 ]
 
 MIDDLEWARE = [
